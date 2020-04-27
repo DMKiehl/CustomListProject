80b86e0be50247d0700515fc7e3fdaa86cf5fbbf
Dana Kiehl <dana.palm87@gmail.com>
2020-04-27 13:44:32 -0500

    Added three Unit Test Methods for the Add Method of the Custom List class

--- CustomList/CustomList.cs
+++ CustomList/CustomList.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace CustomList
 {
-    public class CustomList
+    public class CustomList<T>
     {
+        //member variables
+        private T[] items;
+        //count property
+        //capacity property
+
+        //constructor
+        public CustomList()
+        {
+            items = new T[4];
+        }
+
+        //member methods
+        public void Add(T item)
+        {
+            items[0] = item;
+        }
+
+        public bool Remove(T item)
+        {
+            return false;
+        }
+
     }
 }
--- CustomListUnitTest/UnitTest1.cs
+++ CustomListUnitTest/UnitTest1.cs
@@ -8,8 +8,58 @@ namespace CustomListUnitTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Add_AddingOneValueToEmptyCustomList_AddedValueGoesToIndexZero()
         {
+            //arrange
+            CustomList<int> testList = new CustomList<int>();
+            int itemToAdd = 10;
+            int expected = 10;
+            int actual;
+
+            //act
+            testList.Add(itemToAdd);
+            actual = testList[0];
+
+            //assert
+            Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void Add_AddingOneValueToEmptyCustomList_CountOfCustomListIncrements()
+        {
+            //arrange
+            CustomList<int> testList = new CustomList<int>();
+            int itemToAdd = 10;
+            int expected = 1;
+            int actual;
+
+            //act
+            testList.Add(itemToAdd);
+            actual = testList.Count;
+
+            //assert
+            Assert.AreEqual(expected, actual);
+            
+        }
+
+        [TestMethod]
+        public void Add_AddingMultipleValuesToCutomList_AddedValueGoToIndex()
+        {
+            //arrange
+            CustomList<int> testList = new CustomList<int>();
+            int itemToAdd = 10;
+            int item2ToAdd = 5;
+            int expected = 5;
+            int actual;
+
+            //act
+            testList.Add(itemToAdd);
+            testList.Add(item2ToAdd);
+            actual = testList[1];
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
     }
 }
