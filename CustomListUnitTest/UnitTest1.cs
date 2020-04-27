c82aca545b24ecf3c4d43c1f6f6bb3acfe8bb1d1
Dana Kiehl <dana.palm87@gmail.com>
2020-04-27 14:23:50 -0500

    Added two additional unit test methods for the Add method in the Custom List class.

--- CustomListUnitTest/UnitTest1.cs
+++ CustomListUnitTest/UnitTest1.cs
@@ -61,5 +61,52 @@ namespace CustomListUnitTest
             Assert.AreEqual(expected, actual);
         }
 
+        [TestMethod]
+        public void Add_AddingMultipleValueToCustomList_CountofCustomListIncrements()
+        {
+            //arrange
+            CustomList<int> testList = new CustomList<int>();
+            int itemToAdd = 10;
+            int item2ToAdd = 5;
+            int expected = 2;
+            int actual;
+
+            //act
+            testList.Add(itemToAdd);
+            testList.Add(item2ToAdd);
+            actual = testList.Count;
+
+            //assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void Add_AddingMoreItemsThanInitialCapacityOfListArray_ArrayIndex()
+        {
+            //arrange
+            CustomList<int> testList = new CustomList<int>();
+            int itemToAdd = 10;
+            int itemToAdd2 = 9;
+            int itemToAdd3 = 8;
+            int itemToAdd4 = 7;
+            int itemToAdd5 = 5;
+            int expected = 5;
+            int actual;
+
+            //act
+            testList.Add(itemToAdd);
+            testList.Add(itemToAdd2);
+            testList.Add(itemToAdd3);
+            testList.Add(itemToAdd4);
+            testList.Add(itemToAdd5);
+            actual = testList[4];
+
+
+            //assert
+            Assert.AreEqual(expected, actual);
+
+
+        }
     }
 }
